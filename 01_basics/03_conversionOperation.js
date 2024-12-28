83610614eccacc3d2ef83e5baffcf20324898646
Chinmayi7 <60067411+chinmayi7@users.noreply.github.com>
2024-12-28 04:39:25 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "devi"
+
+console.log(typeof score);
+console.log(typeof(NaN));
+
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleabIsLoggenIn = Boolean(isLoggedIn)
+console.log(booleabIsLoggenIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "devi" => true
+
+
+// Types of coversion
+// 1.String
+// 2.Boolean
+// 3.Number
